52b9aaa28cfd33f2add338e26d6a599713cc38f3
kuldeep <kuldeepsingh.singh77@gmail.com>
2025-01-30 18:51:24 +0530

    updating jwt secret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(private configService: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: 'inventoryasdf1234@secretKey', // Use a secure key in production
+            secretOrKey: configService.get<string>('JWT_SECRET'), // Use a secure key in production
         });
     }
 
